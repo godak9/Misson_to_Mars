f60560dcff47d9c7760833494d679ce2c009f5b4
godak <gabyodak@Gabys-MacBook-Air.local>
2022-08-22 21:53:29 -0400

    Adding Challenge Deliverables

--- scraping.py
+++ scraping.py
@@ -19,6 +19,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "hemispheres_info": hemisphere_image(browser),
         "last_modified": dt.datetime.now()
     }
 
@@ -98,6 +99,25 @@ def mars_facts():
 
     # Convert df into HTML format, add bootstrap
     return df.to_html()
+
+# Scrape Mars Hemsiphere Images
+def hemisphere_image(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    hemisphere_image_urls = []
+    for x in range(4):
+        browser.links.find_by_partial_text('Hemisphere')[x].click()
+        html = browser.html
+        hemi_soup = soup(html,'html.parser')
+        title = hemi_soup.find('h2', class_='title').text
+        img_url = hemi_soup.find('li').a.get('href')
+        hemispheres = {}
+        hemispheres['img_url'] = f'https://marshemispheres.com/{img_url}'
+        hemispheres['title'] = title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
